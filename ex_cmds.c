a00480bbbbdec7c947653cd5cbd831aefa309068
n-t-roff <troff@arcor.de>
2016-06-28 14:57:28 +0200

    doc added

--- ex_cmds.c
+++ ex_cmds.c
@@ -655,7 +655,7 @@ suspend:
 				setNAEOL();
 				ex_printf("@(#) Version 3.7, 10/31/81"
 				    " (4.1cBSD).  git "
-				    "160627 15:54"
+				    "160628 14:55"
 				    + 5);
 				noonl();
 				continue;
